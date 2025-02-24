5f47c7dfe6f39c5acbff79655761171a0f6ad545
ANON-n <thodibar2201@gmail.com>
2025-02-24 10:04:43 +0700

    up code

---
+++ stripstay/src/test/java/com/example/st/stripstay/StripStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.st.stripstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
